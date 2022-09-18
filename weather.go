fb6577104ce6730a28976962e2abcf15feda1dd4
Robert Jacob <xperimental@solidproject.de>
2022-09-18 21:21:17 +0200

    Simplify client code

--- weather.go
+++ weather.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
-	"strings"
 
 	"golang.org/x/oauth2"
 )
@@ -20,23 +19,21 @@ const (
 )
 
 // Config is used to specify credential to Netatmo API
-// ClientID : Client ID from netatmo app registration at http://dev.netatmo.com/dev/listapps
-// ClientSecret : Client app secret
-// Username : Your netatmo account username
-// Password : Your netatmo account password
 type Config struct {
-	ClientID     string
+	// ClientID from netatmo app registration at http://dev.netatmo.com/dev/listapps
+	ClientID string
+	// ClientSecret Client app secret
 	ClientSecret string
-	Username     string
-	Password     string
+	// Username Your netatmo account username
+	Username string
+	// Password Your netatmo account password
+	Password string
 }
 
 // Client use to make request to Netatmo API
 type Client struct {
-	oauth        *oauth2.Config
-	httpClient   *http.Client
-	httpResponse *http.Response
-	Dc           *DeviceCollection
+	oauth      *oauth2.Config
+	httpClient *http.Client
 }
 
 // NewClient create a handle authentication to NetAtmo API
@@ -52,78 +49,40 @@ func NewClient(config Config) (*Client, error) {
 	}
 
 	token, err := oauth.PasswordCredentialsToken(oauth2.NoContext, config.Username, config.Password)
+	if err != nil {
+		return nil, err
+	}
 
 	return &Client{
 		oauth:      oauth,
 		httpClient: oauth.Client(oauth2.NoContext, token),
-		Dc:         &DeviceCollection{},
-	}, err
+	}, nil
 }
 
-// do a url encoded HTTP POST request
-func (c *Client) doHTTPPostForm(url string, data url.Values) (*http.Response, error) {
-	req, err := http.NewRequest("POST", url, strings.NewReader(data.Encode()))
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	return c.doHTTP(req)
-}
-
-// send http GET request
-func (c *Client) doHTTPGet(url string, data url.Values) (*http.Response, error) {
-	if data != nil {
-		url = url + "?" + data.Encode()
-	}
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	return c.doHTTP(req)
-}
-
-// do a generic HTTP request
-func (c *Client) doHTTP(req *http.Request) (*http.Response, error) {
-	var err error
-	c.httpResponse, err = c.httpClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	return c.httpResponse, nil
-}
-
-// process HTTP response
-// Unmarshall received data into holder struct
-func processHTTPResponse(resp *http.Response, err error, holder interface{}) error {
-	defer resp.Body.Close()
-	if err != nil {
-		return err
-	}
-
-	// check http return code
-	if resp.StatusCode != 200 {
-		//bytes, _ := ioutil.ReadAll(resp.Body)
-		return fmt.Errorf("Bad HTTP return code %d", resp.StatusCode)
-	}
-
-	// Unmarshall response into given struct
-	if err = json.NewDecoder(resp.Body).Decode(holder); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// GetStations returns the list of stations owned by the user, and their modules
+// Read returns the list of stations owned by the user and their modules
 func (c *Client) Read() (*DeviceCollection, error) {
-	resp, err := c.doHTTPGet(deviceURL, url.Values{"app_type": {"app_station"}})
+	data := url.Values{"app_type": {"app_station"}}
 
-	if err = processHTTPResponse(resp, err, c.Dc); err != nil {
+	req, err := http.NewRequest("GET", deviceURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.URL.RawQuery = data.Encode()
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("bad HTTP return code: %d", resp.StatusCode)
+	}
+
+	result := &DeviceCollection{}
+	if err = json.NewDecoder(resp.Body).Decode(result); err != nil {
 		return nil, err
 	}
 
-	return c.Dc, nil
+	return result, nil
 }
